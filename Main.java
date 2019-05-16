1f277111306f72dc9f430157c3c52b3340727c88
OwenTrotto <47450694+OwenTrotto@users.noreply.github.com>
2019-05-16 07:26:11 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,19 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Scanner keyboard = new Scanner(System.in);
-        boolean loop = true;
-        while (loop){
-
-        }
-
-
-
-
-
-
-
-
 
 
 
